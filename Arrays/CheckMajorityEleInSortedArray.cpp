903a23e0ecf96f3b054c4c14eed99ab44e1ccf1a
Hemant Pandey <hpscjp@gmail.com>
2017-06-14 16:19:59 +0530

    Create CheckMajorityEleInSortedArray.cpp

---
+++ Arrays/CheckMajorityEleInSortedArray.cpp
@@ -0,0 +1,46 @@
+//Checking majority element greater than n/2 time in a given sorted array
+//Rinning time O(logn)
+//Using concept of Binary Search
+
+/* This function returns true if the x is present more than n/2
+times in arr[] of size n */
+bool isMajority(int arr[], int n, int x)
+{
+    /* Find the index of first occurrence of x in arr[] */
+    int i = _binarySearch(arr, 0, n-1, x);
+ 
+    /* If element is not present at all, return false*/
+    if (i == -1)
+        return false;
+ 
+    /* check if the element is present more than n/2 times */
+    if (((i + n/2) <= (n -1)) && arr[i + n/2] == x)
+        return true;
+    else
+        return false;
+}
+ 
+/* If x is present in arr[low...high] then returns the index of
+first occurrence of x, otherwise returns -1 */
+int _binarySearch(int arr[], int low, int high, int x)
+{
+    if (high >= low)
+    {
+        int mid = (low + high)/2; /*low + (high - low)/2;*/
+ 
+        /* Check if arr[mid] is the first occurrence of x.
+            arr[mid] is first occurrence if x is one of the following
+            is true:
+            (i) mid == 0 and arr[mid] == x
+            (ii) arr[mid-1] < x and arr[mid] == x
+        */
+        if ( (mid == 0 || x > arr[mid-1]) && (arr[mid] == x) )
+            return mid;
+        else if (x > arr[mid])
+            return _binarySearch(arr, (mid + 1), high, x);
+        else
+            return _binarySearch(arr, low, (mid -1), x);
+    }
+ 
+    return -1;
+}
