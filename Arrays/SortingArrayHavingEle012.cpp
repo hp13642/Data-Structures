c1117abe32426369fe5e3362fbf52fed55a849cd
Hemant Pandey <hpscjp@gmail.com>
2017-06-15 01:07:40 +0530

    Create SortingArrayHavingEle012.cpp

--- Arrays/SortingArrayHavingEle012.cpp
+++ Arrays/SortingArrayHavingEle012.cpp
@@ -1,6 +1,20 @@
 //this problem is same as three way partitioning problem
 //OR Dutch National Flag Algorithm, or 3-way Partitioning 
+/*Solution Algorithms-
+a[1..Lo-1] zeroes (0)
+a[Lo..Mid-] ones (1)
+a[Mid..Hi] unknown
+a[Hi+1..N] twos (2)
 
+Steps Involved-
+Lo := 1; Mid := 1; Hi := N;
+while Mid <= Hi do
+    Invariant: a[1..Lo-1]=0 and a[Lo..Mid-1]=1 and a[Hi+1..N]=2; a[Mid..Hi] are unknown.
+    case a[Mid] in
+        0: swap a[Lo] and a[Mid]; Lo++; Mid++
+        1: Mid++
+        2: swap a[Mid] and a[Hi]; Hi–
+*/
 
 // Sort the input array, the array is assumed to
 // have values in {0, 1, 2}
