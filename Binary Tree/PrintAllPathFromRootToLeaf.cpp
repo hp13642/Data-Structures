b9353b873c78fa33bef24d3aeb98745cc63da491
Hemant Pandey <hpscjp@gmail.com>
2017-06-22 11:12:09 +0530

    Create PrintAllPathFromRootToLeaf.cpp

---
+++ Binary Tree/PrintAllPathFromRootToLeaf.cpp
@@ -0,0 +1,51 @@
+/*
+Printing all path from Root to each leaf.
+Algorithm-
+printPathsRecur(tree, path[], pathlen)
+   1) If node is not NULL then 
+         a) push data to path array: 
+                path[pathlen] = node->data.
+         b) increment pathlen 
+                pathlen++
+   2) If node is a leaf node then print the path array.
+   3) Else
+        a) Call printPathsRecur for left subtree
+                 printPathsRecur(node->left, path, pathLen)
+        b) Call printPathsRecur for right subtree.
+                printPathsRecur(node->right, path, pathLen)
+
+*/
+
+/* Recursive helper function -- given a node, and an array containing
+ the path from the root node up to but not including this node,
+ print out all the root-leaf paths. */
+void printPathsRecur(struct node* node, int path[], int pathLen) 
+{
+  if (node==NULL) return;
+ 
+  /* append this node to the path array */
+  path[pathLen] = node->data;
+  pathLen++;
+ 
+  /* it's a leaf, so print the path that led to here */
+  if (node->left==NULL && node->right==NULL) 
+  {
+    printArray(path, pathLen);
+  }
+  else
+  {
+  /* otherwise try both subtrees */
+    printPathsRecur(node->left, path, pathLen);
+    printPathsRecur(node->right, path, pathLen);
+  }
+}
+
+/* Utility that prints out an array on a line */
+void printArray(int ints[], int len)
+{
+  int i;
+  for (i=0; i<len; i++) {
+    printf("%d ", ints[i]);
+  }
+  printf("\n");
+}
