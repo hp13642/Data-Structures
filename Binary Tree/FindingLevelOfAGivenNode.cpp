4e0df7094e0e0ef7c0f5185be66f05d8cf1fd0da
Hemant Pandey <hpscjp@gmail.com>
2017-06-22 21:23:35 +0530

    Create FindingLevelOfAGivenNode.cpp

---
+++ Binary Tree/FindingLevelOfAGivenNode.cpp
@@ -0,0 +1,35 @@
+//Find level of a given node in binary tree assume root is at level 1
+
+/*
+Structure of the node of the binary tree is
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+};
+*/
+// function should return level of the target node
+int getLevel(struct Node *root, int target)
+{
+    if(!root)
+        return 0;
+    queue<Node *> Q;
+    Q.push(root);
+    int level=0;
+    int count;
+    while(!Q.empty()){
+        count=Q.size();
+        level++;
+        while(count--){
+            Node *temp=Q.front();
+            if(temp->data==target)
+                return level;
+            Q.pop();
+            if(temp->left)
+                Q.push(temp->left);
+            if(temp->right)
+                Q.push(temp->right);
+        }
+    }
+}
