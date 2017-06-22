15561a7ca8aa6b4709c455ffd959bafe5758c73e
Hemant Pandey <hpscjp@gmail.com>
2017-06-22 10:26:00 +0530

    Create InorderWithoutRecnStackMorris.cpp

---
+++ Binary Tree/InorderWithoutRecnStackMorris.cpp
@@ -0,0 +1,57 @@
+/*
+Travesing a Binary tree in inorder manner without using recursion and without using stack.
+Algorithm-
+1. Initialize current as root 
+2. While current is not NULL
+   If current does not have left child
+      a) Print current’s data
+      b) Go to the right, i.e., current = current->right
+   Else
+      a) Make current as right child of the rightmost 
+         node in current's left subtree
+      b) Go to this left child, i.e., current = current->left
+
+*/
+
+/* Function to traverse binary tree without recursion and 
+   without stack */
+void MorrisTraversal(struct tNode *root)
+{
+  struct tNode *current,*pre;
+ 
+  if(root == NULL)
+     return; 
+ 
+  current = root;
+  while(current != NULL)
+  {                 
+    if(current->left == NULL)
+    {
+      printf("%d ", current->data);
+      current = current->right;      
+    }    
+    else
+    {
+      /* Find the inorder predecessor of current */
+      pre = current->left;
+      while(pre->right != NULL && pre->right != current)
+        pre = pre->right;
+ 
+      /* Make current as right child of its inorder predecessor */
+      if(pre->right == NULL)
+      {
+        pre->right = current;
+        current = current->left;
+      }
+             
+      /* Revert the changes made in if part to restore the original 
+        tree i.e., fix the right child of predecssor */   
+      else 
+      {
+        pre->right = NULL;
+        printf("%d ",current->data);
+        current = current->right;      
+      } /* End of if condition pre->right == NULL */
+    } /* End of if condition current->left == NULL*/
+  } /* End of while */
+}
