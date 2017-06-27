08d240ad22d3deac9c17f04db57fa00a03a060fc
Hemant Pandey <hpscjp@gmail.com>
2017-06-28 00:38:42 +0530

    Update RotateArrayRightByK.cpp

--- Arrays/RotateArrayRightByK.cpp
+++ Arrays/RotateArrayRightByK.cpp
@@ -4,7 +4,7 @@
 
 
 
-
+//in O(n) using O(k) extra space
  void rotate(vector<int>& nums, int k) {
         stack<int> st;
         int i,n=nums.size();
@@ -23,3 +23,29 @@
             nums[i]=t;
         }
     }
+
+
+//In Java using constant space
+public void rotate(int[] nums, int k) {
+
+    if(nums == null || nums.length < 2){
+        return;
+    }
+    
+    k = k % nums.length;
+    reverse(nums, 0, nums.length - k - 1);
+    reverse(nums, nums.length - k, nums.length - 1);
+    reverse(nums, 0, nums.length - 1);
+    
+}
+
+private void reverse(int[] nums, int i, int j){
+    int tmp = 0;       
+    while(i < j){
+        tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+        i++;
+        j--;
+    }
+}
